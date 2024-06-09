8fe86bc9c0135a3a4287d94190f33599b628c061
Tamas Dukai <tomi.2@hotmail.co.uk>
2024-06-09 13:20:37 +0200

    app.vue a-tagekhez target="_blank" ,hogy új ablakban nyíljanak meg az oldalak fb,insta,x

--- Legovizsga-main/src/App.vue
+++ Legovizsga-main/src/App.vue
@@ -89,14 +89,14 @@ import { RouterView } from "vue-router";
           <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Lego Inc</span>
         </div>
         <ul class="nav col-md-4 pe-5 justify-content-end list-unstyled d-flex">
-          <li class="ms-3"><a class=" links text-body-secondary" href="https://x.com/LEGO_Group">
+          <li class="ms-3"><a class=" links text-body-secondary" href="https://x.com/LEGO_Group" target="_blank">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-twitter-x"
                 viewBox="0 0 16 16">
                 <path
                   d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
                 <use xlink:href="#"></use>
               </svg>X-Twitter</a></li>
-          <li class="ms-3"><a class=" links text-body-secondary" href="https://www.instagram.com/lego/" >
+          <li class="ms-3"><a class=" links text-body-secondary" href="https://www.instagram.com/lego/" target="_blank" >
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-instagram"
                 viewBox="0 0 16 16">
                 <path
@@ -111,7 +111,7 @@ import { RouterView } from "vue-router";
                 <use xlink:href="#"></use>
               </svg>Instagram</a></li>
           <!-- <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi bi-facebook" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li> -->
-          <li class="ms-3"><a class="links text-body-secondary" href="https://www.facebook.com/LEGO/">
+          <li class="ms-3"><a class="links text-body-secondary" href="https://www.facebook.com/LEGO/" target="_blank">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook"
                 viewBox="0 0 16 16">
                 <path
