d3ea27167c7dc29d180c63aaf1d8c30ce011eec0
Tamas Dukai <tomi.2@hotmail.co.uk>
2024-06-09 20:42:12 +0200

    próbálkozás a frontend regisztrációhoz

--- Legovizsga-main/src/views/RegistrationView.vue
+++ Legovizsga-main/src/views/RegistrationView.vue
@@ -1,31 +1,96 @@
 <script setup>
   
-import {ref} from 'vue'
+import {ref} from 'vue';
+import axios from 'axios';
 
-const userName = ref("")
-const email = ref("")
-const password = ref("")
-const error = ref("")
-const buttonDisabled = ref(false)
+
+
+const userName = ref("");
+const email = ref("");
+const password = ref("");
+const error = ref("");
+const buttonDisabled = ref(false);
+const password_confirmation = ref('');
+const errors = ref({});
+
+
 const ell = ()=> {
   console.log('ok')
   if(password.value.length >= 8)
   buttonDisabled.value = false
-}
-const ellenor = ()=>{
-  console.log('ok ok ok')
-  if (password.value.length < 8){
-    error.value = ("A jelszónak legalább 8 karakter hosszúnak kell lennie!")
-    // buttonDisabled.value= true
+};
+// const ellenor = ()=>{
+//   console.log('ok ok ok')
+//   if (password.value.length < 8){
+//     error.value = ("A jelszónak legalább 8 karakter hosszúnak kell lennie!")
+//     // buttonDisabled.value= true
+//   }
+// else{
+//   buttonDisabled.value = false
+//   error.value = ""
+// }}
+
+const ellenor = () => {
+  if (password.value.length < 8) {
+    error.value = 'A jelszónak legalább 8 karakter hosszúnak kell lennie!';
+  } else {
+    buttonDisabled.value = false;
+    error.value = '';
   }
-else{
-  buttonDisabled.value = false
-  error.value = ""
-}
+};
 
-}
+const register = () => {
+  axios.post('http://127.0.0.1:8000/api/register', {
+    name: userName.value,
+    email: email.value,
+    password: password.value,
+    password_confirmation: password_confirmation.value,
+  })
+  .then(response => {
+    console.log('Registration successful', response.data);
+    // Redirect or notify the user about successful registration
+  })
+  .catch(error => {
+    if (error.response && error.response.status === 422) {
+      errors.value = error.response.data.errors;
+    }
+  });
+};
 
 
+
+// script verzió nem pedig script setup
+// import axios from 'axios';
+
+// export default {
+//   data() {
+//     return {
+//       name: '',
+//       email: '',
+//       password: '',
+//       password_confirmation: '',
+//       errors: {}
+//     };
+//   },
+//   methods: {
+//     register() {
+//       axios.post('http://127.0.0.1:8000/api/register', {
+//         name: this.name,
+//         email: this.email,
+//         password: this.password,
+//         password_confirmation: this.password_confirmation
+//       })
+//       .then(response => {
+//         console.log('Registration successful', response.data);
+//       })
+//       .catch(error => {
+//         if (error.response.status === 422) {
+//           this.errors = error.response.data.errors;
+//         }
+//       });
+//     }
+//   }
+// };
 
 </script>
 
@@ -36,8 +101,8 @@ else{
     <div class="conatiner">
       <form class="row g-3" @submit.prevent="onsubmit">
       <div class="col-md-12">
-        <label for="inputEmail4" class="form-label">Felhasználó neve:</label>
-        <input v-model="userName" required type="text" class="form-control" id="inputEmail4" placeholder="Kérem ide írja be a felhasználónevét!">
+        <label for="inputName" class="form-label">Felhasználó neve:</label>
+        <input v-model="userName" required type="text" class="form-control" id="inputName" placeholder="Kérem ide írja be a felhasználónevét!">
       </div>
       <br>
       <div class="col-md-12">
