7e7390e7d12e7488719ac732611c8db00310a124
Tamas Dukai <tomi.2@hotmail.co.uk>
2024-06-15 23:40:11 +0200

    changed fb és insta svg-k + width height 22-ről 20-ra

--- Legovizsga-main/src/views/App2.vue
+++ Legovizsga-main/src/views/App2.vue
@@ -115,6 +115,7 @@ import { RouterView } from "vue-router";
                 d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
               <use xlink:href="#"></use>
             </svg>X-Twitter</a></li>
+            
         <li class="ms-3"><a class=" links text-body-secondary" href="https://www.instagram.com/lego/" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-instagram"
               viewBox="0 0 16 16">
@@ -129,6 +130,7 @@ import { RouterView } from "vue-router";
         2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
               <use xlink:href="#"></use>
             </svg>Instagram</a></li>
+
         <!-- <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi bi-facebook" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li> -->
         <li class="ms-3"><a class="links text-body-secondary" href="https://www.facebook.com/LEGO/" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook"
